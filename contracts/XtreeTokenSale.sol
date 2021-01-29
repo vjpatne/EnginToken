9031708b02e37bbf20c031c5c54c451ff00cdcf0
vijay <vjpatne@gmail.com>
2021-01-29 19:58:36 +0530

    intial commit

---
+++ contracts/XtreeTokenSale.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.14;
+
+import "./XtreeToken.sol";
+
+contract XtreeTokenSale {
+    address payable admin ;
+    XtreeToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(XtreeToken _tokenContract, uint256 _tokenPrice) public {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+
+        // UPDATE: Let's not destroy the contract here
+        // Just transfer the balance to the admin
+        admin.transfer(address(this).balance);
+    }
+
+    function updateTokenPrice(uint256 _tvalue) public {
+        require(msg.sender == admin);
+        tokenPrice = _tvalue;
+    }
+}
